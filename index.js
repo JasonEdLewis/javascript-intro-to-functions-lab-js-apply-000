e42d97d51e0e99cbc9001fd9b21fcacdf72c7b0d
Jason Lewis <jasonedlewis@gmail.com>
2019-06-15 15:43:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ let logWhisper= (string)=> {
 };
 let sayHiToGrandma = (string) => {
   if (string === string.toLowerCase()){
-    return 'I cant hear you!'
+    return 'I can't hear you!'
   }else if (string === string.toUpperCase()){
-    return 'YES INDEED';
+    return 'YES INDEED!';
   }else if(string === "I love you, Grandma."){
     return "I love you, too."
   }
